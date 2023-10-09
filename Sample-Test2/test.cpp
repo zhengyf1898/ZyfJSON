452beb088d508f9596c828c865cf6c5b17188cb4
zhengyf1898 <zhengyf1898@163.com>
2023-10-09 16:55:41 +0800

    解析数字测试通过

--- Sample-Test2/test.cpp
+++ Sample-Test2/test.cpp
@@ -3,10 +3,61 @@
 #include <gtest/gtest.h>
 #include <string>
 
+#define testLiteral(value, expect)     \
+  do                                   \
+  {                                    \
+    Parser p(value);                   \
+    EXPECT_EQ(p.parse(), expect);      \
+  } while (0)
+
+
+#define testNumber(expect, value)       \
+  do                                    \
+  {                                     \
+    Parser p(value);                    \
+	p.parse();                          \
+    EXPECT_EQ(p.getParseNum(), expect); \
+  } while (0)
 
 TEST(RoundTrip, literal)
 {
-	std::string t = " true ";
-	Parser p(t);
-	EXPECT_EQ(p.parse(), 0);
-}
+	testLiteral("true", 0);
+	testLiteral("false", 0);
+	testLiteral("null", 0);
+
+};
+
+TEST(RoundTrip, number)
+{
+
+	testNumber(0.0, "0");
+	testNumber(0.0, "-0");
+	testNumber(0.0, "-0.0");
+	testNumber(1.0, "1");
+	testNumber(-1.0, "-1");
+	testNumber(1.5, "1.5");
+	testNumber(-1.5, "-1.5");
+	testNumber(3.1416, "3.1416");
+	testNumber(1E10, "1E10");
+	testNumber(1e10, "1e10");
+	testNumber(1E+10, "1E+10");
+	testNumber(1E-10, "1E-10");
+	testNumber(-1E10, "-1E10");
+	testNumber(-1e10, "-1e10");
+	testNumber(-1E+10, "-1E+10");
+	testNumber(-1E-10, "-1E-10");
+	testNumber(1.234E+10, "1.234E+10");
+	testNumber(1.234E-10, "1.234E-10");
+	testNumber(5.0E-324, "5e-324");
+	testNumber(0, "1e-10000");
+	testNumber(1.0000000000000002, "1.0000000000000002");
+	testNumber(4.9406564584124654e-324, "4.9406564584124654e-324");
+	testNumber(-4.9406564584124654e-324, "-4.9406564584124654e-324");
+	testNumber(2.2250738585072009e-308, "2.2250738585072009e-308");
+	testNumber(-2.2250738585072009e-308, "-2.2250738585072009e-308");
+	testNumber(2.2250738585072014e-308, "2.2250738585072014e-308");
+	testNumber(-2.2250738585072014e-308, "-2.2250738585072014e-308");
+	testNumber(1.7976931348623157e+308, "1.7976931348623157e+308");
+	testNumber(-1.7976931348623157e+308, "-1.7976931348623157e+308");
+
+};
--- ZyfJSON/JSON.h
+++ ZyfJSON/JSON.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include <stdexcept>
+#include<ctype.h>
 
 using namespace std;
 
@@ -22,19 +24,29 @@ public:
 
 		int res = 0;
 		parseWhitespace();
-		res = parseValue();
-		//parseWhitespace();
+		if ((res = parseValue()) == LEPT_PARSE_OK) {
+			parseWhitespace();
+			if (*curr_ != '\0')
+				return LEPT_PARSE_ROOT_NOT_SINGULAR;
+		}
+		
 		return res;
 	}
+
+	double getParseNum() { return val; }
 
 private:
 
 	void parseWhitespace() {
-		while (*curr_ == ' ' || *curr_ == '\t' || *curr_ == '\n' || *curr_ == '\r') {
-			curr_++;
+		try {
+			while (*curr_ == ' ' || *curr_ == '\t' || *curr_ == '\n' || *curr_ == '\r')
+				curr_++;
 		}
-		start_ = curr_;
+		catch(...){
+			start_ = curr_;
+		}
 	}
+
 	int parseValue() {
 		switch (*curr_) {
 			case 'n':
@@ -43,11 +55,48 @@ private:
 				return parseLiteral("true");
 			case 'f':
 				return parseLiteral("false");
+			case '\0':
+				return LEPT_PARSE_EXPECT_VALUE;
+			default:
+				return parseNumber();
 		}
+	}
+	int parseNumber() {
+		if (*curr_ == '-')
+			++curr_;
+		if (*curr_ == '0')
+			++curr_;
+		else
+		{
+			if (!isdigit(*curr_))
+				return LEPT_PARSE_INVALID_VALUE;
+			while (isdigit(*++curr_))
+				;
+		}
+		if (*curr_ == '.')
+		{
+			if (!isdigit(*++curr_))
+				return LEPT_PARSE_INVALID_VALUE;
+			while (isdigit(*++curr_))
+				;
+		}
+		if (toupper(*curr_) == 'E')
+		{
+			++curr_;
+			if (*curr_ == '-' || *curr_ == '+')
+				++curr_;
+			if (!isdigit(*curr_))
+				return LEPT_PARSE_INVALID_VALUE;
+			while (isdigit(*++curr_))
+				;
+		}
+		val = strtod(&*start_, nullptr);
+		start_ = curr_;
+		return LEPT_PARSE_OK;
 	}
 
 	int parseLiteral(const std::string &literal) {
-		for (int i = 0; literal[i ]; i++, curr_++) {
+		for (int i = 0; literal[i]; i++, curr_++) {
 			if (*curr_ != literal[i])
 				return LEPT_PARSE_INVALID_VALUE;
 		}
@@ -57,5 +106,6 @@ private:
 	string::iterator start_;
 	string::iterator curr_;
 	string content_;
+	double val;
 };
 
